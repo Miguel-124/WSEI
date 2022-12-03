702b882b389e657ce2905daa505374a99f631c02
Miguel-124 <michalgorecki124@gmail.com>
2022-12-04 00:58:24 +0100

    Finally some fresh start

---
+++ WDE/Projekty/Project1/Project1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ZADANIE 8.2.1
+            Console.WriteLine("Input number of cykles: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] tabs;
+            
+            for(int y=0;y<n;y++)
+            {
+                Console.WriteLine($"Input {y+1} cykles: ");
+                tabs[y] = Console.ReadLine();
+            }
+            Array.Sort(tabs);
+            int max = Convert.ToInt32(tabs[0]);
+            int min = Convert.ToInt32(tabs[n]);
+            //ZADANIE 8.2.2
+            //ZADANIE 8.2.3
+            //ZADANIE 8.2.4
+            //ZADANIE 8.2.5
+            //ZADANIE 8.2.6
+            //ZADANIE 8.2.7
+            //ZADANIE 8.2.8
+
+            //ZADANIE 8.3.1
+            //ZADANIE 8.3.2
+            //ZADANIE 8.3.3
+            //ZADANIE 8.3.4
+
+            //ZADANIE 8.4.1
+            //ZADANIE 8.4.2
+            //ZADANIE 8.4.3
+            //ZADANIE 8.4.4
+            //ZADANIE 8.4.5
+            //ZADANIE 8.4.6
+            //ZADANIE 8.4.7
+            //ZADANIE 8.4.8
+
+            //ZADANIE 8.5.1
+            //ZADANIE 8.5.2
+
+            //ZADANIE 8.6.1
+            //ZADANIE 8.6.2
+            //ZADANIE 8.6.3
+            //ZADANIE 8.6.4
+
+        }
+    }
+}
